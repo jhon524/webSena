be15810bdece06f47c91d4f75438a87d5d79a1b5
jhon524 <jagalejo10@gmail.com>
2026-05-28 20:02:46 -0500

    cambios modelo

--- apps/contenido/models.py
+++ apps/contenido/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-from usuarios.models import Usuario
+from apps.usuarios.models import Usuario
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
